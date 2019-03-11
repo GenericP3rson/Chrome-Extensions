73640c8426860f2daa35fcdfc7d69db701fa4120
Paromita Datta <schaudhary@s.keystoneschool.org>
2019-03-11 09:27:06 -0500

    Taking stuff off of my old computer

---
+++ Folder1.2/chromeContent.js
@@ -0,0 +1,10 @@
+//var a = Math.table(Math.random()*3);
+//if (a === 0) {
+    alert("Hello, Dad-Dad and Mom-Mom!!!");
+//} else {
+    //if (a === 1) {
+        alert("Ar-ar, you're so arisome!!!");
+    //} else {
+        alert("She-she, you are the greatest!!!");
+    //}
+//}
